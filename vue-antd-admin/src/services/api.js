50eae36eaf8554f4950776c9864aa814f385516a
stone <123376536@qq.com>
2020-11-11 14:47:42 +0800

    sss服务器提交

--- vue-antd-admin/src/services/api.js
+++ vue-antd-admin/src/services/api.js
@@ -1,6 +1,8 @@
 //跨域代理前缀
 const API_PROXY_PREFIX='http://localhost:8080/admin'
-const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://go.yizheshuo.top/admin' : API_PROXY_PREFIX
+//const BASE_URL = 'http://localhost:8080/admin'
+
 // const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
   LOGIN: `${BASE_URL}/login`,
